a505ebfb5c8ff37c183bdf05cd41260b6b880432
Olim Saidov <olim.mail@gmail.com>
2020-09-19 17:00:28 +0500

    Upgrade stellar sdk

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clostellar "0.1.0-SNAPSHOT"
+(defproject clostellar "0.2.0-SNAPSHOT"
   :description "Stellar Java SDK wrapper"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.github.stellar/java-stellar-sdk "0.4.1"]]
+                 [com.github.stellar/java-stellar-sdk "0.18.0"]]
   :repositories [["jitpack" "https://jitpack.io"]])
